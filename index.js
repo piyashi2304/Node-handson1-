34d281817e0d2b50008f4b6a357b3cb102f0df09
piyashi2304 <piyashisaha@gmail.com>
2023-11-10 21:42:10 +0530

    handson1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jsonData=require('./jsonpkg')
 const server = http.createServer((request,response)=>{
     if(request.url==='/'){
         response.write("<title>Handson1- Home server</title> <h1>What is Node Js</h1><p>Node.js is an open-source, cross-platform JavaScript runtime environment that executes JavaScript code outside of a web browser</p>")
+        response.write("<p>Benifit of Node.js</p><ol><li>High Performance</li><li>Asynchronous and Non-blocking</li><li>Scalability</li><li>Large Ecosystem (NPM)</li>")
         response.end()
     }
     if(request.url==='/student'){
